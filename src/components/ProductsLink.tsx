b7309309ec06353224c284e4bd23ac38c749e13f
Rinidh <rinidhdhokia@gmail.com>
2023-12-14 21:36:53 +0300

    Prevent popover from fading away when hovering over it / away from link

--- src/components/ProductsLink.tsx
+++ src/components/ProductsLink.tsx
@@ -1,4 +1,4 @@
-import { Box, Link as ChakraLink } from "@chakra-ui/react";
+import { Box, Link as ChakraLink, Fade } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import ProductsPopOver from "./ProductsPopOver";
 import { useState } from "react";
@@ -16,13 +16,22 @@ const ProductsLink = () => {
         as={Link}
         to={"/x"}
         colorScheme={"valaBlue"}
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
+        onMouseEnter={() => setIsHovered(true)} //set only the mouse-enter prop
       >
         Products
       </ChakraLink>
 
-      <ProductsPopOver isHovered={isHovered} />
+      <Fade in={isHovered}>
+        <Box
+          bg={"blue"}
+          boxSize={"xs"}
+          onMouseLeave={() => setIsHovered(false)} //set only the mouse-leave prop
+          style={{ position: "absolute", top: "30px", left: "0px" }}
+          /* this position is in relation to the relative position at the parent ie ProductsLink */
+        >
+          <ProductsPopOver />
+        </Box>
+      </Fade>
     </Box>
   );
 };
--- src/components/ProductsPopOver.tsx
+++ src/components/ProductsPopOver.tsx
@@ -1,23 +1,7 @@
-import { Box, Container } from "@chakra-ui/react";
+// this file will only hold the content of the popover
 
-interface Props {
-  isHovered: boolean;
-}
-
-const ProductsPopOver = ({ isHovered }: Props) => {
-  return (
-    <>
-      <Box
-        bg={"blue"}
-        opacity={isHovered ? 1 : 0}
-        boxSize={"xs"}
-        style={{ position: "absolute", top: "30px", left: "0px" }}
-        /* this position is in relation to the relative position at the parent ie ProductsLink */
-      >
-        CDE
-      </Box>
-    </>
-  );
+const ProductsPopOver = () => {
+  return <>ABCD</>;
 };
 
 export default ProductsPopOver;
